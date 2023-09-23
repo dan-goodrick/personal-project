4ae1e10ede5ddf7aa71483fea248829014439dcc
dan-goodrick <dannyjeee@yahoo.com>
2023-09-23 15:51:02 -0600

    Cleans up some stff

--- server/db.js
+++ server/db.js
@@ -6,8 +6,8 @@ async function connectToDB(dbURI) {
   const sequelize = new Sequelize(dbURI, {
     logging: console.log, // set logging: false to disable outputting SQL queries to console
     define: {
-      timestamps: false, // don't want created_at or updated_at columns
-      underscored: true, // use snake_case rather than camelCase column names
+      timestamps: false,
+      underscored: true,
     },
   });
 
