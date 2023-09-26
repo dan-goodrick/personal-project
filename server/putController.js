3407ccea575049bbbd677aebb8def53e25d1cd50
dan-goodrick <dannyjeee@yahoo.com>
2023-09-26 16:06:47 -0600

    removed cloudinary keys

--- server/putController.js
+++ server/putController.js
@@ -4,15 +4,7 @@ import {
   Person,
 } from "./model.js";
 
-// import {v2 as cloudinary} from 'cloudinary';
-// cloudinary.config({
-//   cloud_name: 'dyozbgxgo',
-//   api_key: '344111862392384',
-//   api_secret: 'SGjvV5AbnS83AcV-rD1TBUHby4I'
-// });
-// cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-//   { public_id: "olympic_flag" },
-//   function(error, result) {console.log(result); });
+
 
 const serverFunctions = {
   
