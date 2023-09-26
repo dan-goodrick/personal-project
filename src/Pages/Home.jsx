30548c1f0e1bfc4a01dfc2253caa8c45319c406d
dan-goodrick <dannyjeee@yahoo.com>
2023-09-26 12:07:48 -0600

    adds boilerplate carousel

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,33 @@
 import { useLoaderData } from "react-router-dom";
 import ViewCandidate from "../Elements/ViewCandidate";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
 
 //TODO: add top card for each category past, current fundraising
 
 export default function Home() {
   const { projects } = useLoaderData();
-  // console.log("fundraising", projects);
+
+  
+  const images = [
+    {
+      original: "https://picsum.photos/id/1018/1000/600/",
+      thumbnail: "https://picsum.photos/id/1018/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1015/1000/600/",
+      thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/1000/600/",
+      thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+  ];
+
   return (
     <>
       <h1>Builders of Hope</h1>
+      <ImageGallery items={images} />
       {projects.map((candidate) => (
         <ViewCandidate key={candidate.candidateId} candidate={candidate} />
         ))}
