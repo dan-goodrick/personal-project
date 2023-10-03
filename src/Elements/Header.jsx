86300756e396083c6c55a6141b1aa0bbb3df843f
dan-goodrick <dannyjeee@yahoo.com>
2023-10-03 13:39:13 -0600

    adds styling for header

--- src/Elements/Header.jsx
+++ src/Elements/Header.jsx
@@ -1,12 +1,53 @@
 // import { useEffect } from "react"
 import { NavLink, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-// import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import {
+  AppBar,
+  Avatar,
+  Container,
+  Divider,
+  Hidden,
+  IconButton,
+  Link,
+  List,
+  ListItem,
+  SwipeableDrawer,
+  Toolbar,
+} from "@mui/material";
+import { makeStyles } from "@mui/styles";
 import axios from "axios";
+import { ChevronRight, Menu } from "@mui/icons-material";
+
+const navLinks = [
+  {
+    name: "Current Fundraiser",
+    shortName: "Fundraiser",
+    route: "/fundraising",
+  },
+  { name: "Upcoming Projects", shortName: "Upcoming", route: "/plannedBuilds" },
+  { name: "Completed Builds", shortName: "Completed", route: "/pastProjects" },
+  { name: "About Us", shortName: "About", route: "/aboutUs" },
+];
+
+const useStyles = makeStyles(() => ({
+  link: {
+    marginRight: 20,
+  },
+  avatar: {
+    marginRight: "auto",
+    backgroundColor: "black",
+    borderRadius: 0,
+    height: 30,
+    border: "2px solid black",
+  },
+}));
 
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const styles = useStyles();
+  const [open, setOpen] = useState(false);
   const userId = useSelector((state) => state.userId);
 
   const handleLogout = () => {
@@ -19,27 +60,143 @@ const Header = () => {
       .catch((err) => console.error("Logout Error", err));
   };
 
-  // TODO Login/Letter SVG with dropdown to logout
   return (
-    <div>
-      <NavLink to="/">BoH</NavLink>
-      <NavLink to="/fundraising">Current Fundraiser</NavLink>
-      <NavLink to="/plannedBuilds">Upcoming Projects</NavLink>
-      <NavLink to="/pastProjects">Previous Builds</NavLink>
-      <NavLink to="/aboutUs">About Us</NavLink>
-      {userId ? (
-        <>
-          <NavLink to="/admin">Admin</NavLink>
-          <NavLink>
-            <button onClick={handleLogout}>Logout</button>
-          </NavLink>
-        </>
-      ) : (
-        <>
-          <NavLink to="/login">Log in/Register</NavLink>
-        </>
-      )}
-    </div>
+    <AppBar position="sticky" color="default">
+      <Container maxWidth="md">
+        <Toolbar disableGutters>
+          <Avatar src={"/logo.webp"} href="/" className={styles.avatar} />
+          <Hidden smDown>
+            {navLinks.map((item) => (
+              <ListItem key={item.name}>
+                <Link
+                  className={styles.link}
+                  color="textPrimary"
+                  variant="button"
+                  underline="none"
+                  href={item.route}
+                >
+                  {item.name}
+                </Link>
+              </ListItem>
+            ))}
+            {userId ? (
+              <>
+                <ListItem>
+                  <Link
+                    className={styles.link}
+                    color="textPrimary"
+                    variant="button"
+                    underline="none"
+                    href="/admin"
+                  >
+                    Admin
+                  </Link>
+                </ListItem>
+                <ListItem>
+                  <Link
+                    className={styles.link}
+                    color="textPrimary"
+                    variant="button"
+                    underline="none"
+                    onClick={handleLogout}
+                  >
+                    Logout
+                  </Link>
+                </ListItem>
+              </>
+            ) : (
+              <ListItem>
+                <Link
+                  className={styles.link}
+                  color="textPrimary"
+                  variant="button"
+                  underline="none"
+                  href="/login"
+                >
+                  Log in
+                </Link>
+              </ListItem>
+            )}
+          </Hidden>
+          <Hidden smUp>
+            <IconButton onClick={() => setOpen(true)}>
+              <Menu />
+            </IconButton>
+          </Hidden>
+        </Toolbar>
+      </Container>
+      <SwipeableDrawer
+        anchor="right"
+        open={open}
+        onOpen={() => setOpen(true)}
+        onClose={() => setOpen(false)}
+      >
+        <div
+          onClick={() => setOpen(false)}
+          onKeyPress={() => setOpen(false)}
+          role="button"
+          tabIndex={0}
+        >
+          <IconButton>
+            <ChevronRight />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {navLinks.map((item) => (
+            <ListItem key={item.name}>
+              <Link
+                className={styles.link}
+                color="textPrimary"
+                variant="button"
+                underline="none"
+                href={item.route}
+              >
+                {item.shortName}
+              </Link>
+            </ListItem>
+          ))}
+            {userId ? (
+              <>
+                <ListItem>
+                  <Link
+                    className={styles.link}
+                    color="textPrimary"
+                    variant="button"
+                    underline="none"
+                    href="/admin"
+                  >
+                    Admin
+                  </Link>
+                </ListItem>
+                <ListItem>
+                  <Link
+                    className={styles.link}
+                    color="textPrimary"
+                    variant="button"
+                    underline="none"
+                    onClick={handleLogout}
+                  >
+                    Logout
+                  </Link>
+                </ListItem>
+              </>
+            ) : (
+              <ListItem>
+                <Link
+                  className={styles.link}
+                  color="textPrimary"
+                  variant="button"
+                  underline="none"
+                  href="/login"
+                >
+                  Log in
+                </Link>
+              </ListItem>
+            )}
+        </List>
+      </SwipeableDrawer>
+    </AppBar>
   );
 };
 
