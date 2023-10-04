52ab1f48bacb27426951a21fa51c729a3de6e714
dan-goodrick <dannyjeee@yahoo.com>
2023-10-03 20:25:41 -0600

    adds a basic hero - not styled

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,20 +1,31 @@
 import { useLoaderData } from "react-router-dom";
-import ViewCard from "../Elements/ViewCard";
 import ImageGallery from "react-image-gallery";
-import "react-image-gallery/styles/css/image-gallery.css";
 
-//TODO: add top card for each category past, current fundraising
 
 export default function Home() {
-  const { projects, images } = useLoaderData();
-
+  const { images } = useLoaderData();
   return (
     <>
-      <h1>Builders of Hope</h1>
-      <ImageGallery items={images} autoPlay={true} />
-      {projects.map((candidate, i) => (
-        <ViewCard key={i} family={candidate} />
-        ))}
+
+    <header style={{ paddingLeft: 0 }}>
+      <div
+        className='p-5 text-center bg-image'
+        style={{ backgroundImage: "url('landscape.jpeg')", height: 375 }}
+      >
+        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)', height: 375 }}>
+          <div className='d-flex justify-content-center align-items-center h-100'>
+            <div className='text-white'>
+              <h1 className='mb-3'>Builders of Hope</h1>
+              <h4 className='mb-3'>Because we have been given much</h4>
+              <a className='btn btn-outline-light btn-lg' href='#!' role='button'>We too can give!</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div className='d-flex justify-content-center align-items-center h-100' style={{ padding: 10 }}>
+    <ImageGallery items={images} autoPlay={true} />
+    </div>
     </>
   );
 }
