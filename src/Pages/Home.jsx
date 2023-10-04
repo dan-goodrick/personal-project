d80669a1e9420d07c8e457ae6d4a50aa53196aef
dan-goodrick <dannyjeee@yahoo.com>
2023-10-04 10:58:16 -0600

    sets width of carousel

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,15 @@
 import { useLoaderData } from "react-router-dom";
 import ImageGallery from "react-image-gallery";
 import Hero from "../Elements/Hero";
-
+import { Box, Container, Stack } from "@mui/material";
+import 'react-image-gallery/styles/css/image-gallery.css';
 
 export default function Home() {
   const { images } = useLoaderData();
   return (
-    <>
+    <Stack container spacing={2}>
     <Hero />
-    {/* <ImageGallery items={images} autoPlay={true} /> */}
-    </>
+    <ImageGallery items={images} autoPlay={true} sx={{width:"100vw"}}/>
+    </Stack>
   );
 }
