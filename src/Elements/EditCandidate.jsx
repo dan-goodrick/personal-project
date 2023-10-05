3d53488fbb6d36b3412e8103a5c79e55e2956839
dan-goodrick <dannyjeee@yahoo.com>
2023-10-05 15:08:15 -0600

    adds flex to edit candidate

--- src/Elements/EditCandidate.jsx
+++ src/Elements/EditCandidate.jsx
@@ -2,13 +2,13 @@ import CandidateData from "./CandidateData";
 import PersonCard from "./PersonCard";
 
 
-export default function EditCandidate({ width, candidate, setEditCandidate, handleEditCandidate }) {
+export default function EditCandidate({ candidate, setEditCandidate, handleEditCandidate }) {
   
   // console.log("EditCandidate", candidate);
   return (
     <>
-      {candidate.people.map((person, i) => <PersonCard width={width} key={i} person={person} i={i} /> )}
-      <CandidateData width={width} candidate={candidate} setEditCandidate={setEditCandidate} handleEditCandidate={handleEditCandidate} />
+      {candidate.people.map((person, i) => <PersonCard key={i} person={person} i={i} /> )}
+      <CandidateData  candidate={candidate} setEditCandidate={setEditCandidate} handleEditCandidate={handleEditCandidate} />
     </>
   );
 }
