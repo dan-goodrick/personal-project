a1410f80f5c88bd6f73914bcbdd139055be70a28
dan-goodrick <dannyjeee@yahoo.com>
2023-11-06 11:27:09 -0700

    updated aboutUS

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,28 +1,27 @@
 import { useLoaderData } from "react-router-dom";
 import ViewCard from "../Elements/ViewCard";
 import { Grid, Typography } from "@mui/material";
-import { Background } from "./Home";
 
 export default function AboutUs() {
   const { boardMembers } = useLoaderData();
   return (
     <div >
-      <Background />
-      <Grid container direction="column" spacing={2}>
+
+      <Grid container direction="column" spacing={1}>
         <Grid item xs={12} m={4}>
           <Typography variant="h3" fontWeight={600} color="text" align={"center"}>
-            What our volunteers are saying
+            The Builders of Hope
           </Typography>
         </Grid>
         <Grid
           container
           item
           direction="row"
-          spacing={5}
+          spacing={1}
           justifyContent="center"
         >
           {boardMembers.map((member, i) => (
-            <Grid key={i} item xs={12} sm={3} mx={2}>
+            <Grid key={i} item xs={12} sm={3} m={1}>
               <ViewCard family={member} crop={false} />
             </Grid>
           ))}
